3874b69ae404257745a1d7a3b5d68a09eff37681
mfonette <ettemfon98@gmail.com>
2020-07-29 17:10:03 +0200

    Second update

--- first.js
+++ first.js
@@ -1,12 +1,15 @@
 const name = document.getElementById('name')
 const date = document.getElementById('date');
 var pin = document.getElementById('pin');
-var inp = document.getElementById('myInput');
+var pwd = document.getElementById('myInput');
+
 
 // TODO: Remove error message once field is valid
+// done that
+
 
 // function to validate name
-function validateName() {
+validateName = () => {
     const re = /^[a-zA-Z]{2,}$/; 
     // console.log(re)
     if (re.test(name.value)){
@@ -23,8 +26,8 @@ function validateName() {
 
 
 //  function to validate the length of the pin
-function validatePin() {
-    console.log(e);
+validatePin = () => {
+    // console.log(e);
    if(pin.value.length < 4){
     pin.classList.add('is-invalid');
        console.log()
@@ -37,14 +40,14 @@ function validatePin() {
 }
 
 
-//  function to validate each pin entering the input field
+//  function to validate each key entering the input field
 pin.addEventListener('keypress', (e) => {
-    // e.key.innerHTML = "a"
-    var char = e.key
+    pin.type="password"
+    let char = e.key
     console.log(`${char}`)
-    var pinIsValid =  validatePin()
-    var charRegex = /[0-9]|Backspace|#/;
-    var charIsValid = charRegex.test(char)
+    let pinIsValid =  validatePin()
+    let charRegex = /[0-9]|Backspace/;
+    let charIsValid = charRegex.test(char)
     console.log(`${charIsValid}`)
     
     if (charIsValid && pinIsValid) {
@@ -58,55 +61,93 @@ pin.addEventListener('keypress', (e) => {
     }
  });
 
-
 //  TODO: try to add this to the previous keypress, one keypress event needed
- pin.addEventListener('keypress', (e) => {
-     pin.type="password"
- })
+//  why are you stressing my life
 
+// FUNCTION TO VALIDATE DATE FORMAT
+validateDate = () => {
+    let dateCharRegex = /(1[6-9][0-9]{2}|2[0-9]{3})[/](0[1-9]|1[0-2])[/]([0-2]{1}[0-9]{1}|3[0-1])/;
+    // let dateCharRegex = /([2-9]{1}[0-9]{3})[/](0[1-9]|1[0-2])[/]([0-2]{1}[0-9]{1}|3[0-1])/;
+    let dateCharRegexIsValid = dateCharRegex.test(date.value)
+    console.log(`${dateCharRegexIsValid}`)
 
-//  Function to validate Date
-function validateDate(e) {
-    const re = /([0-9]{4})[/](0[1-9]|1[0-2])[/]([0-2]{1}[0-9]{1}|3[0-1])/;
-
-    if (re.test(date.value)){
-        date.classList.remove('is-invalid');
-        console.log('good to go')
-        return true
-        // alert( 'follow the format');
-    }
-    else{
+    if (!dateCharRegexIsValid){
         date.classList.add('is-invalid');
         console.log('follow the format')
-        e.preventDefault();
+        return 
     }
 }
+
+// FUNCTION TO VALIDATE EACH KEY ENTERED ON THE DATE INPUT
+date.addEventListener('keypress', (e) => {
+    let dateChar = e.key
+    console.log(`${dateChar}`)
+    let eDateCharRegex = /[0-9]|[/]|Backspace/
+    let dateCharIsValid = eDateCharRegex.test(dateChar)
+    console.log(`${dateCharIsValid}`)
+
+    if (!dateCharIsValid) {
+        e.preventDefault();
+        console.log('good to go')
+        return 
+    }
+})
 
 
 // function to validate password
-function validatePassword() {
-    if(inp.value.length < 8){
-        inp.classList.add('is-invalid');
+validatePassword = () => {
+    if(pwd.value.length < 8){
+        pwd.classList.add('is-invalid');
         console.log('less than 8')
     }
     else{
-        inp.classList.remove('is-invalid');    }
+        pwd.classList.remove('is-invalid');   
+     }
 }
 
-function myFunction() {
-    if (inp.type === "password") {
-        inp.type = "text";
+// function onFocus for password
+valPassword = () => {
+    // if(pwd.classList.contains('is-invalid')) {
+        pwd.classList.remove('is-invalid')
+    // }
+}
+
+// function onFocus for date
+valdate = () => {
+    if(date.classList.contains('is-invalid')) {
+        date.classList.remove('is-invalid')
+    }
+}
+
+// function onFocus for name
+valname= () => {
+    if(name.classList.contains('is-invalid')) {
+        name.classList.remove('is-invalid')
+    }
+}
+
+// function onFocus for pin
+valpin = () => {
+    if(pin.classList.contains('is-invalid')) {
+        pin.classList.remove('is-invalid')
+    }
+}
+
+myFunction = () => {
+    if (pwd.type === "password") {
+        pwd.type = "text";
             }
     else {
-        inp.type = "password";
+        pwd.type = "password";
         }
 }
 
 // he said comment anything just to change the file
 // Seriously?
+// lmao...oh yes
 
 // Form Blur Event listener
 name.addEventListener('blur', validateName);
-inp.addEventListener('blur', validatePassword);
+pwd.addEventListener('blur', validatePassword);
 date.addEventListener('blur', validateDate);
 pin.addEventListener('blur', validatePin);
